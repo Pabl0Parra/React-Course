f88936f9eb6050ee0310fb4f4ff470c627d178c3
Pabl0Parra <pabsblue16@gmail.com>
2022-10-27 16:53:59 +0200

    added last js review lesson

---
+++ src/bases/12-ternary-op.js
@@ -0,0 +1,17 @@
+const active = true;
+
+// let message = "";
+
+// if (!active) {
+//   message = "Active";
+// } else {
+//   message = "Innactive";
+// }
+
+// Regular ternary
+// const message = active ? "Active" : "Innactive";
+
+// Shorthand
+const message = active && "Active";
+
+console.log(message);
